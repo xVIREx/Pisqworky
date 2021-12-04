08573ebd842cf6a82627e34dbc912a1249a4a43b
Patrik Cihal <xxv1rexx@gmail.com>
2021-12-04 12:22:27 +0100

    port 3000

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = 3000;
 app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Application running on port ${port}!`);
