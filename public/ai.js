29ae717118f39c4535e2992e8471d7097c766609
Patrik Cihal <xxv1rexx@gmail.com>
2021-12-04 12:21:37 +0100

    update

---
+++ public/ai.js
@@ -0,0 +1,92 @@
+"use strict";
+function getBestMove(data) {
+    let player = 1;
+    let bestMove = [5, -1];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i] != 0)
+            continue;
+        data[i] = player;
+        let score = evaluateMove(data, i, player);
+        if (score < bestMove[0]) {
+            bestMove[0] = score;
+            bestMove[1] = i;
+        }
+        data[i] = 0;
+    }
+    return bestMove[1];
+}
+function flr(v) {
+    return v > 0 ? Math.floor(v) : Math.ceil(v);
+}
+function isCorner(p, offsetV) {
+    let pV = [p % gs, flr(p / gs)];
+    for (let i = 0; i < 2; i++) {
+        pV[i] += offsetV[i];
+        if (pV[i] == gs || pV[i] == -1)
+            return true;
+    }
+    return false;
+}
+function evaluateMove(data, pos, player) {
+    let priority = 5;
+    for (let i = 0; i < offsets.length; i++) {
+        function value(p) {
+            return data[pos + p * offsets[i]];
+        }
+        let l = [-5, 5];
+        let _l = [0, 0];
+        let opp = player == 1 ? 2 : 1;
+        let count = -1;
+        for (let j = 0; j < 2; j++) {
+            let dir = j * 2 - 1;
+            for (let k = 0; Math.abs(k) < 5; k += dir) {
+                let e = false;
+                switch (value(k)) {
+                    case opp:
+                        e = true;
+                        break;
+                    case 0:
+                        if (_l[j] == 0)
+                            _l[j] = k;
+                        break;
+                    case player:
+                        if (_l[j] == 0)
+                            count++;
+                        break;
+                }
+                if (e || isCorner(pos + k * offsets[i], offsetsV[i * 2 + j])) {
+                    l[j] = k + (+!e) * dir;
+                    break;
+                }
+                ;
+            }
+            if (_l[j] == 0)
+                _l[j] = l[j];
+        }
+        function getPriority(p, r, d, c, s) {
+            for (; Math.abs(p) < Math.abs(l[+(d > 0)]) && value(p) == 0; p += d) {
+                c--;
+                s++;
+                if (c == 0)
+                    return s;
+            }
+            for (; Math.abs(p) < Math.abs(l[+(d > 0)]) && value(p) == player; p += d) {
+                c--;
+                if (c == 0)
+                    return s;
+            }
+            if (p == l[+(d > 0)] && r == l[+!(d > 0)])
+                return s;
+            if (p == l[+(d > 0)])
+                return getPriority(r, p, -d, c, s);
+            if (r == l[+!(d > 0)])
+                return getPriority(p, r, d, c, s);
+            return Math.min(getPriority(p, r, d, c, s), getPriority(r, p, -d, c, s));
+        }
+        if (count >= 5)
+            return 0;
+        if (l[1] - l[0] > 5)
+            priority = Math.min(Math.min(getPriority(_l[0], _l[1], -1, 5 - count, 0), getPriority(_l[1], _l[0], 1, 5 - count, 0)), priority);
+    }
+    return priority;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express = require('express');
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use(express.static('public'))
 
