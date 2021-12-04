8928bfb286f5588a6769d4320e2515913796adfa
Patrik Cihal <xxv1rexx@gmail.com>
2021-12-04 12:35:09 +0100

    port 80:

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 app.use(express.static('public'));
 app.listen(port, () => {
     console.log(`Application running on port ${port}!`);
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(express.static('public'))
 
